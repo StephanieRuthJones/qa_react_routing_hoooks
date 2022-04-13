565bc679f85b58e6a2db2d2f5562218653793c78
Stephanie Jones <stephanieruthjones@gmail.com>
2022-04-13 14:36:01 -0600

    remove all code, just boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import Dog from './components/Dog'
+import './App.css'
+
 function App() {
   return (
     <div className="App">
-      <Dog />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
